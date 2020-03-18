3ecd5991b131004ff38cc49ddf6c416b4cf9978d
Alexander Ivanov <Alexander.Ivano@x5.ru>
2020-03-18 22:35:08 +0300

    Тесты Авторизации

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@
 
 use App\User;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /*
@@ -19,10 +20,8 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'nickname' => $faker->unique()->userName,
+        'password' => Hash::make(123321),
+        'api_token' => Str::random(36),
     ];
 });
