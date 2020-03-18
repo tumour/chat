e79c912a33654a9c3f4badd6fc4e1732d4374e21
Alexander Ivanov <Alexander.Ivano@x5.ru>
2020-03-18 20:08:58 +0300

    Изменение роутов группы auth

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('auth/register', 'Auth\RegisterController@register')->middleware('guest');
+Route::post('auth/login', 'Auth\LoginController@login')->middleware('guest');
